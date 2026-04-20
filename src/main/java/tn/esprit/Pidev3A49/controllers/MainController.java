ba2121a3b7b09432f56b3f7b5063b80dbd881ca1
emnabh05 <bhemna05@gmail.com>
2026-04-20 02:13:59 +0100

    Fix: ajout import BorderPane

--- src/main/java/tn/esprit/Pidev3A49/controllers/MainController.java
+++ src/main/java/tn/esprit/Pidev3A49/controllers/MainController.java
@@ -25,6 +25,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
